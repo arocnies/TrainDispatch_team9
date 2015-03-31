80a8f65cd8f144cda60f0963713c132c58314fa4
Jennifer <jnguy2011@my.fit.edu>
2015-03-31 16:48:38 -0400

    Added in the gsu-ui jars and fixed VisualizationTest.java

--- test/VisualizationTest.java
+++ test/VisualizationTest.java
@@ -10,10 +10,10 @@ public class VisualizationTest {
 
 	public static void main (String args []) {
         Graph graph = new SingleGraph("TestInput");
-
+        System.setProperty("gs.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 
         // This line does not produce the expected result... why? (See 'http://graphstream-project.org/doc/Tutorials/GraphStream-CSS-Reference_1.2/')
-        graph.addAttribute("ui.stylesheet", "node { text-alignment: under; text-style: bold; text-color: white; text-background-mode: rounded-box; text-background-color: red; text-padding: 1px; text-offset: 0px, 2px; } ");
+        //graph.addAttribute("ui.stylesheet", "node { text-alignment: under; text-style: bold; text-color: white; text-background-mode: rounded-box; text-background-color: black; text-padding: 1px; text-offset: 0px, 2px; } ");
 
         graph.Graph myGraph = GraphFactory.generateGraph("res/test.graph");
         System.out.println(myGraph);
@@ -45,6 +45,14 @@ public class VisualizationTest {
 		// Displays labels for all nodes.
 		for (Node n : graph) {
 			n.addAttribute("ui.label", n.getId());
+            //n.addAttribute("ui.stylesheet", "node { text-alignment: under; } ");
+
+            //n.addAttribute("ui.stylesheet", "node { text-style: bold; } ");
+            //n.addAttribute("ui.stylesheet", "node { text-color: white; } ");
+            //n.addAttribute("ui.stylesheet", "node { text-background-mode: rounded-box; } ");
+            //n.addAttribute("ui.stylesheet", "node { text-background-color: red; } ");
+            //n.addAttribute("ui.stylesheet", "node { text-padding: 1px; } ");
+            //n.addAttribute("ui.stylesheet", "node { text-offset: 0px, 2px; } ");
 		}
 
         // Test animation of all paths.
@@ -77,6 +85,8 @@ public class VisualizationTest {
 
                 // Remove colors from graph.
                 graph.removeAttribute("ui.stylesheet");
+                graph.addAttribute("ui.stylesheet", "node { text-alignment: under; text-style: bold; text-color: white; text-background-mode: rounded-box; text-background-color: black; text-padding: 1px; text-offset: 0px, 2px; } ");
+
             }
         }
     }
