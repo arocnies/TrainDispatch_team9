4d19c7855fcf90c326563541b48ba601f70a9f53
arocnies <arocnies@gmail.com>
2015-04-21 16:16:41 -0400

    DispatchTest now uses argument for graph file. Minor string changes.

--- test/JFreeChartTest.java
+++ test/JFreeChartTest.java
@@ -29,7 +29,7 @@ public class JFreeChartTest extends ApplicationFrame
         JFreeChart xylineChart = ChartFactory.createXYLineChart(
                 chartTitle,
                 "Trains",
-                "Delay",
+                "Performance",
                 createDataset( m , n ),
                 PlotOrientation.VERTICAL,
                 true, true, false);
