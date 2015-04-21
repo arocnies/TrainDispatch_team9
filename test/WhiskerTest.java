17f2b2c1df1d1cbaf2ec000f6033db89eceb3ec0
Jennifer Nguy <jnguy2011>
2015-04-21 01:34:57 -0400

    Whiskers for whiskers...  Attempt to make a whiskers plot.  [Not a working class, see comments for ideas.]

---
+++ test/WhiskerTest.java
@@ -0,0 +1,120 @@
+/**
+ * Created by J. Nguy on 4/21/2015.
+*/
+import dispatch.BaseCaseDispatch;
+import dispatch.Dispatch;
+import dispatch.Plan;
+import dispatch.Schedule;
+import graph.Graph;
+import graph.GraphFactory;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+//import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+
+
+public class WhiskerTest extends ApplicationFrame
+{
+    public WhiskerTest( String applicationTitle, String chartTitle, Graph m, int n )
+    {
+
+        // Setup.
+        super(applicationTitle);
+        JFreeChart xylineChart = ChartFactory.createXYLineChart(
+                chartTitle,
+                "Trains",
+                "Delay",
+                createDataset( m , n ),
+                PlotOrientation.VERTICAL,
+                true, true, false);
+
+        ChartPanel chartPanel = new ChartPanel( xylineChart );
+        chartPanel.setPreferredSize( new java.awt.Dimension( 1200 , 700 ) );
+        final XYPlot plot = xylineChart.getXYPlot();
+        setContentPane( chartPanel );
+    }
+
+    private XYDataset createDataset( Graph m , int trials)
+    {
+        final DefaultStatisticalCategoryDataset bcSeries = new DefaultStatisticalCategoryDataset();
+        //final XYSeries slSeries  = new XYSeries( "SingleLocking" );
+        //final XYSeries opSeries  = new XYSeries( "Optimized" );
+
+        // Loops through train amounts.
+        for (int i = 0; i <= 1000; i += 50) {
+
+            double bcSum = 0;
+            //double slSum = 0;
+            //double opSum = 0;
+
+            // Loop through trails.
+            for (int j = trials; j > 0; j--) {
+                Schedule bcSchedule = new Schedule(i, m.getNodes(), 1000);
+                //Schedule slSchedule = new Schedule(i, m.getNodes(), 1000);
+                //Schedule opSchedule = new Schedule(i, m.getNodes(), 1000);
+
+                Dispatch bcDispatch = new BaseCaseDispatch(m);
+                //Dispatch slDispatch = new Dispatch(m);
+                //Dispatch opDispatch = new OptimizedDispatch(m);
+
+                Plan bcPlan = bcDispatch.dispatchTrains(bcSchedule);
+                //Plan slPlan = slDispatch.dispatchTrains(slSchedule);
+                //Plan opPlan = opDispatch.dispatchTrains(opSchedule);
+
+                bcSum += bcPlan.getAverageDelay();
+                //slSum += slPlan.getAverageDelay();
+                //opSum += opPlan.getAverageDelay();
+            }
+            bcSeries.add(i, 1 / (bcSum / trials), "BaseCase", "Test");
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ DefaultStatisticalCategoryDataset()
+
+ add(double mean, double standardDeviation, java.lang.Comparable rowKey, java.lang.Comparable columnKey)
+
+http://www.jfree.org/jfreechart/api/javadoc/org/jfree/data/statistics/DefaultStatisticalCategoryDataset.html
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+            //slSeries.add(i, 1 / (slSum / trials));
+            //opSeries.add(i, 1 / (opSum / trials));
+        }
+
+
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(bcSeries);
+        //dataset.addSeries(slSeries);
+        //dataset.addSeries(opSeries);
+
+        return dataset;
+    }
+
+
+
+    public static void main( String[ ] args )
+    {
+        Graph graph = GraphFactory.generateGraph(args[0]);
+        String title = args[0];
+
+        int tests = Integer.parseInt(args[1]);
+        long start = System.currentTimeMillis();
+
+
+        WhiskerTest chart = new WhiskerTest("Analysis", title, graph, tests);
+        chart.pack();
+        RefineryUtilities.centerFrameOnScreen(chart);
+        chart.setVisible(true);
+
+        System.out.println("Tests = " + tests);
+        System.out.println("Runtime = " + (System.currentTimeMillis() - start) / 1000.0);
+    }
+}
